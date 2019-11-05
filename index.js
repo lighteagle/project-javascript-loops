7751a98fbb7190e6345f99d1288303c01460d1de
lighteagle <sofyan.agus18@gmail.com>
2019-11-05 16:05:01 +0700

    update

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ do {
 for (let index = 0; index <= 100; index += 10) {
   console.log(index)
 }
+
+let input = prompt("Enter Factorial Number :")
+let factorialNumber = 1
+for (let i = 1; i <= input; i++) {
+  factorialNumber *= i
+}
+console.log(`
+Input : ${input}
+Output :${factorialNumber}`)
+
+let inputName = prompt("Enter Your Name :")
+// let arrayName = inputName.split("")
+// // arrayName.reverse()
+// // let reverseName = ''
+// // for (let i = 0; i < arrayName.length; i++) {
+// //   reverseName += arrayName[i]
+// // }
+
+let reverseName = inputName.split('').reverse().join('')
+console.log(reverseName)
